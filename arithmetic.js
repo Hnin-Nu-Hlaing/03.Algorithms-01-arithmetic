c9a259d61a4da2ec73603378d4a08b875d81df05
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-05-12 16:12:31 +0630

    arithmetic file

---
+++ arithmetic.js
@@ -0,0 +1,21 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    console.log(num1 + num2)
+};
+add( 14 ,34);
+
+var subtract = function(num1, num2) {
+    console.log(num1 - num2);
+};
+subtract(52, 32);
+
+var multiply = function(num1, num2) {
+    console.log(num1 * num2);
+};
+multiply(12,11);
+
+var divide = function(num1, num2) {
+    console.log(num1 / num2);
+};
+divide(284,13);
